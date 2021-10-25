74ca1120a0a97ccfdeeec7cecd669026378b6c80
khenlyasparin <khenly.asparin@gsfe.tupcavite.edu.ph>
2021-10-25 20:08:30 +0800

    added urlpattern for menu.html

--- canteensystem/urls.py
+++ canteensystem/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     # read views.py and execute index function
+    # read views.py and execute menu function
     path('', views.index, name='index'),
+    path('menu', views.menu, name='menu'),
 
 ]
