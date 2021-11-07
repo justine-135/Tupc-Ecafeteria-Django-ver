dd292a1ae0eb3d144ac19cc39786fa659e6f410e
Justine <ujusine@yahoo.com>
2021-11-07 15:06:58 +0800

    modified database options

--- elective2/settings.py
+++ elective2/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'elective2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'elective2_django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
