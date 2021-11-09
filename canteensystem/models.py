7ae8202d4fdb2e23ffe8eb9ebbb3dc48b0a86515
khenlyasparin <khenly.asparin@gsfe.tupcavite.edu.ph>
2021-11-09 21:10:37 +0800

    Added Lunchmeal, Addons, and Drinks models

--- canteensystem/models.py
+++ canteensystem/models.py
@@ -8,3 +8,21 @@ class Breakfast_table(models.Model):  # Table for Breakfast
     img = ImageField(max_length=100, verbose_name="img")
     quantity = IntegerField(verbose_name="quantity")
     price = DecimalField(max_digits=4, decimal_places=2, verbose_name="price")
+
+class Lunchmeal_table(models.Model):  # Table for Lunch
+    name = CharField(max_length=30, verbose_name="name")
+    img = ImageField(max_length=100, verbose_name="img")
+    quantity = IntegerField(verbose_name="quantity")
+    price = DecimalField(max_digits=4, decimal_places=2, verbose_name="price")
+
+class Addons_table(models.Model):  # Table for Addons
+    name = CharField(max_length=30, verbose_name="name")
+    img = ImageField(max_length=100, verbose_name="img")
+    quantity = IntegerField(verbose_name="quantity")
+    price = DecimalField(max_digits=4, decimal_places=2, verbose_name="price")
+
+class Drinks_table(models.Model):  # Table for Drinks
+    name = CharField(max_length=30, verbose_name="name")
+    img = ImageField(max_length=100, verbose_name="img")
+    quantity = IntegerField(verbose_name="quantity")
+    price = DecimalField(max_digits=4, decimal_places=2, verbose_name="price")
