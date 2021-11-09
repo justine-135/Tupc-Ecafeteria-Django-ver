27792885a0133dd319e498b922527f2c11b1ea1b
Justine <ujusine@yahoo.com>
2021-11-09 20:53:34 +0800

    created breakfast model

--- canteensystem/models.py
+++ canteensystem/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField, DecimalField, IntegerField
+from django.db.models.fields.files import ImageField
 
-# Create your models here.
+
+class Breakfast_table(models.Model):  # Table for Breakfast
+    name = CharField(max_length=30, verbose_name="name")
+    img = ImageField(max_length=100, verbose_name="img")
+    quantity = IntegerField(verbose_name="quantity")
+    price = DecimalField(max_digits=4, decimal_places=2, verbose_name="price")
