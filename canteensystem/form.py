94d48e94eec01a4f6fb2a73fc635463f9960d25d
Justine <ujusine@yahoo.com>
2021-11-13 23:35:31 +0800

    update

--- canteensystem/form.py
+++ canteensystem/form.py
@@ -2,7 +2,25 @@ from django import forms
 from .models import *
 
 
-class FormMenu(forms.ModelForm):
+class BreakfastForm(forms.ModelForm):
     class Meta:
         model = Breakfast_table
-        fields = '__all__'
+        fields = ['item_name', 'item_img', 'item_price', 'item_quantity']
+
+
+# class LunchmealForm(forms.ModelForm):
+#     class Meta:
+#         model = Lunchmeal_table
+#         fields = '__all__'
+
+
+# class DrinksForm(forms.ModelForm):
+#     class Meta:
+#         model = Drinks_table
+#         fields = '__all__'
+
+
+# class AddonsForm(forms.ModelForm):
+#     class Meta:
+#         model = Addons_table
+#         fields = '__all__'
