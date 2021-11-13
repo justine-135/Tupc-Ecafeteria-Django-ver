5781cfe7995bbf1386edaed94721a5ac6010f1c5
Justine <ujusine@yahoo.com>
2021-11-13 23:35:41 +0800

    update

--- canteensystem/models.py
+++ canteensystem/models.py
@@ -4,25 +4,29 @@ from django.db.models.fields.files import ImageField
 
 
 class Breakfast_table(models.Model):  # Table for Breakfast
-    name = CharField(max_length=30, verbose_name="name")
-    img = ImageField(max_length=100, verbose_name="img")
-    quantity = IntegerField(verbose_name="quantity")
-    price = DecimalField(max_digits=4, decimal_places=2, verbose_name="price")
 
-class Lunchmeal_table(models.Model):  # Table for Lunch
-    name = CharField(max_length=30, verbose_name="name")
-    img = ImageField(max_length=100, verbose_name="img")
-    quantity = IntegerField(verbose_name="quantity")
-    price = DecimalField(max_digits=4, decimal_places=2, verbose_name="price")
+    item_name = CharField(max_length=30, verbose_name="item_name")
+    item_img = ImageField(max_length=100, verbose_name="item_img")
+    item_quantity = IntegerField(verbose_name="item_quantity")
+    item_price = DecimalField(max_digits=4, decimal_places=2, verbose_name="item_price")
 
-class Addons_table(models.Model):  # Table for Addons
-    name = CharField(max_length=30, verbose_name="name")
-    img = ImageField(max_length=100, verbose_name="img")
-    quantity = IntegerField(verbose_name="quantity")
-    price = DecimalField(max_digits=4, decimal_places=2, verbose_name="price")
 
-class Drinks_table(models.Model):  # Table for Drinks
-    name = CharField(max_length=30, verbose_name="name")
-    img = ImageField(max_length=100, verbose_name="img")
-    quantity = IntegerField(verbose_name="quantity")
-    price = DecimalField(max_digits=4, decimal_places=2, verbose_name="price")
+# class Lunchmeal_table(models.Model):  # Table for Lunch
+#     item_name = CharField(max_length=30, verbose_name="item_name")
+#     item_img = ImageField(max_length=100, verbose_name="item_img")
+#     item_quantity = IntegerField(verbose_name="item_quantity")
+#     item_price = DecimalField(max_digits=4, decimal_places=2, verbose_name="item_price")
+
+
+# class Addons_table(models.Model):  # Table for Addons
+#     item_name = CharField(max_length=30, verbose_name="item_name")
+#     item_img = ImageField(max_length=100, verbose_name="item_img")
+#     item_quantity = IntegerField(verbose_name="item_quantity")
+#     item_price = DecimalField(max_digits=4, decimal_places=2, verbose_name="item_price")
+
+
+# class Drinks_table(models.Model):  # Table for Drinks
+#     item_name = CharField(max_length=30, verbose_name="item_name")
+#     item_img = ImageField(max_length=100, verbose_name="item_img")
+#     item_quantity = IntegerField(verbose_name="item_quantity")
+#     item_price = DecimalField(max_digits=4, decimal_places=2, verbose_name="item_price")
