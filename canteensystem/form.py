16afa1b6183acb44090a10bd39975dd428836f2c
Justine <ujusine@yahoo.com>
2021-11-14 00:32:24 +0800

    renamed model

--- canteensystem/form.py
+++ canteensystem/form.py
@@ -4,23 +4,6 @@ from .models import *
 
 class BreakfastForm(forms.ModelForm):
     class Meta:
-        model = Breakfast_table
+        model = Menus
         fields = ['item_name', 'item_img', 'item_price', 'item_quantity']
 
-
-# class LunchmealForm(forms.ModelForm):
-#     class Meta:
-#         model = Lunchmeal_table
-#         fields = '__all__'
-
-
-# class DrinksForm(forms.ModelForm):
-#     class Meta:
-#         model = Drinks_table
-#         fields = '__all__'
-
-
-# class AddonsForm(forms.ModelForm):
-#     class Meta:
-#         model = Addons_table
-#         fields = '__all__'
