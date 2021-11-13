3430cfbf829ca032ec24dc0c6bb6113ce8bfe321
Justine <ujusine@yahoo.com>
2021-11-14 00:33:15 +0800

    removed unecessary models and renamed Breakfast_table to Menus

--- canteensystem/models.py
+++ canteensystem/models.py
@@ -3,30 +3,9 @@ from django.db.models.fields import CharField, DecimalField, IntegerField
 from django.db.models.fields.files import ImageField
 
 
-class Breakfast_table(models.Model):  # Table for Breakfast
+class Menus(models.Model):  # Table for Breakfast
 
     item_name = CharField(max_length=30, verbose_name="item_name")
-    item_img = ImageField(max_length=100, verbose_name="item_img")
+    item_img = ImageField(max_length=100, verbose_name="item_img", blank=True)
     item_quantity = IntegerField(verbose_name="item_quantity")
-    item_price = DecimalField(max_digits=4, decimal_places=2, verbose_name="item_price")
-
-
-# class Lunchmeal_table(models.Model):  # Table for Lunch
-#     item_name = CharField(max_length=30, verbose_name="item_name")
-#     item_img = ImageField(max_length=100, verbose_name="item_img")
-#     item_quantity = IntegerField(verbose_name="item_quantity")
-#     item_price = DecimalField(max_digits=4, decimal_places=2, verbose_name="item_price")
-
-
-# class Addons_table(models.Model):  # Table for Addons
-#     item_name = CharField(max_length=30, verbose_name="item_name")
-#     item_img = ImageField(max_length=100, verbose_name="item_img")
-#     item_quantity = IntegerField(verbose_name="item_quantity")
-#     item_price = DecimalField(max_digits=4, decimal_places=2, verbose_name="item_price")
-
-
-# class Drinks_table(models.Model):  # Table for Drinks
-#     item_name = CharField(max_length=30, verbose_name="item_name")
-#     item_img = ImageField(max_length=100, verbose_name="item_img")
-#     item_quantity = IntegerField(verbose_name="item_quantity")
-#     item_price = DecimalField(max_digits=4, decimal_places=2, verbose_name="item_price")
+    item_price = IntegerField(verbose_name="item_price")
