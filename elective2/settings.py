d0f6c444e542f27893f940771bbec8dccc7f1453
Justine <ujusine@yahoo.com>
2021-11-14 01:47:30 +0800

    created temporary folders for images uploaded add folder to static folder

--- elective2/settings.py
+++ elective2/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/temp_img/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/temp_img')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
