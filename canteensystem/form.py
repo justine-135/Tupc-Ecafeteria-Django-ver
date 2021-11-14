793eb282951039e960244f213fb7e07037c172d6
louisesebastianeyagi <louisesebastiane.yagi@gsfe.tupcavite.edu.ph>
2021-11-15 00:37:53 +0800

    added placeholder to forms

--- canteensystem/form.py
+++ canteensystem/form.py
@@ -6,4 +6,6 @@ class BreakfastForm(forms.ModelForm):
     class Meta:
         model = Menus
         fields = ['item_name', 'item_img', 'item_price', 'item_quantity']
-
+        widgets = {'item_name': forms.TextInput(attrs={'placeholder': 'Enter food'}),
+        'item_price': forms.TextInput(attrs={'placeholder': 'Enter Price'}),
+        'item_quantity': forms.TextInput(attrs={'placeholder': 'Enter Quantity'})}
