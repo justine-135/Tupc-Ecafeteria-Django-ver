6f22ddb0b97db731153f09f546cf15e3cde86457
Justine <ujusine@yahoo.com>
2021-11-16 18:53:38 +0800

    added dropdown

--- canteensystem/models.py
+++ canteensystem/models.py
@@ -2,11 +2,20 @@ from django.db import models
 from django.db.models.fields import CharField, DecimalField, IntegerField
 from django.db.models.fields.files import ImageField
 
+# gawin mo tong FOOD_CATEGORY
+# palitan mo ng 
+COLOR_CHOICES = (
+    ('green','GREEN'), #breakfast 'Breakfast'
+    ('blue', 'BLUE'), #lunchmeal 'Lunchmeal'
+    ('red','RED'), #drinks 'Beverage and Drinks'
+    ('orange','ORANGE'),#addons 'Add-ons'
+    ('black','BLACK'),#remove to
+)
 
-class Menus(models.Model):  # Table for Breakfast
-
+class Menus(models.Model):
+    item_categories = CharField(max_length=6, choices=COLOR_CHOICES, default='green') #change morin default sa 'breakfast'
     item_name = CharField(max_length=30, verbose_name="item_name")
-    item_img = ImageField(max_length=100, verbose_name="item_img", blank=True)
     item_quantity = IntegerField(verbose_name="item_quantity")
     item_price = IntegerField(verbose_name="item_price")
-    item_categories = CharField(max_length=40, verbose_name="item_categories")
+    item_img = ImageField(max_length=100, verbose_name="item_img", blank=True)
+
