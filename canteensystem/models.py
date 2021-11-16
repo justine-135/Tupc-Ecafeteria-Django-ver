eabeb367333e7624f18331e8a3fd28936febab07
louisesebastianeyagi <louisesebastiane.yagi@gsfe.tupcavite.edu.ph>
2021-11-16 15:42:44 +0800

    added table for category

--- canteensystem/models.py
+++ canteensystem/models.py
@@ -9,4 +9,4 @@ class Menus(models.Model):  # Table for Breakfast
     item_img = ImageField(max_length=100, verbose_name="item_img", blank=True)
     item_quantity = IntegerField(verbose_name="item_quantity")
     item_price = IntegerField(verbose_name="item_price")
-    item_categories = CharField(max_length=30, verbose_name="item_categories")
+    item_categories = CharField(max_length=40, verbose_name="item_categories")
