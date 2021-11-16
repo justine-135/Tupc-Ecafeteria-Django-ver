fd6003e84ff4ba64e829cd43a8ac772f76eed2f7
khenlyasparin <khenly.asparin@gsfe.tupcavite.edu.ph>
2021-11-16 19:11:23 +0800

    Added Food Category

--- canteensystem/models.py
+++ canteensystem/models.py
@@ -1,19 +1,17 @@
 from django.db import models
-from django.db.models.fields import CharField, DecimalField, IntegerField
+from django.db.models.fields import CharField, IntegerField
 from django.db.models.fields.files import ImageField
 
-# gawin mo tong FOOD_CATEGORY
-# palitan mo ng 
-COLOR_CHOICES = (
-    ('green','GREEN'), #breakfast 'Breakfast'
-    ('blue', 'BLUE'), #lunchmeal 'Lunchmeal'
-    ('red','RED'), #drinks 'Beverage and Drinks'
-    ('orange','ORANGE'),#addons 'Add-ons'
-    ('black','BLACK'),#remove to
+
+FOOD_CATEGORY = (
+    ('breakfast','Breakfast'),
+    ('lunchmeal', 'Lunchmeal'),
+    ('drinks','Beverage and Drinks'), 
+    ('addons','Add-ons'),
 )
 
 class Menus(models.Model):
-    item_categories = CharField(max_length=6, choices=COLOR_CHOICES, default='green') #change morin default sa 'breakfast'
+    item_categories = CharField(max_length=6, choices=FOOD_CATEGORY, default='breakfast')
     item_name = CharField(max_length=30, verbose_name="item_name")
     item_quantity = IntegerField(verbose_name="item_quantity")
     item_price = IntegerField(verbose_name="item_price")
