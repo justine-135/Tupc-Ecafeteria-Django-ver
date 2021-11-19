8bb3caa47868ec062f65164bebb3431cdd157f06
louisesebastianeyagi <louisesebastiane.yagi@gsfe.tupcavite.edu.ph>
2021-11-20 00:43:52 +0800

    added new class for orders in models

--- canteensystem/models.py
+++ canteensystem/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.fields import CharField, IntegerField
+from django.db.models.fields import CharField, DateTimeField, IntegerField
 from django.db.models.fields.files import ImageField
 
 
@@ -10,6 +10,11 @@ FOOD_CATEGORY = (
     ('addons','Add-ons'),
 )
 
+TRUE_FALSE_CHOICES = (
+    (True, 'Yes'),
+    (False, 'No')
+)
+
 class Menus(models.Model):
     item_categories = CharField(max_length=20, choices=FOOD_CATEGORY, default='breakfast')
     item_name = CharField(max_length=30, verbose_name="item_name")
@@ -17,3 +22,9 @@ class Menus(models.Model):
     item_price = IntegerField(verbose_name="item_price")
     item_img = ImageField(max_length=100, verbose_name="item_img", blank=True)
 
+class Orders(models.Model):
+    time = DateTimeField(verbose_name='time', blank=True)
+    item = CharField(max_length=30, verbose_name="item")
+    total_purchase = IntegerField(verbose_name="total_purchase")
+    item_status = CharField(max_length=30, verbose_name="item_status")
+    cancel_order = models.CharField(max_length=30, choices = TRUE_FALSE_CHOICES)
