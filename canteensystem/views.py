361aad5b0b95f043aa02976e59d2c511ed8ba2a7
Justine <ujusine@yahoo.com>
2021-11-20 18:22:13 +0800

    save data to Orders model from menu.html

--- canteensystem/views.py
+++ canteensystem/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .form import Menu
-from .models import Menus
+from .models import Menus, Orders
 from django.conf import settings
 
 
@@ -9,13 +9,11 @@ def index(request):
     form = Menu()
     datas = Menus.objects.all()
 
-    #filter menu
     drinks = Menus.objects.filter(item_categories="drinks")
     addons = Menus.objects.filter(item_categories="addons")
     breakfast = Menus.objects.filter(item_categories="breakfast")
     lunchmeal = Menus.objects.filter(item_categories="lunchmeal")
 
-    #add filtered here
     context = {'form': form, "foods": datas, 'drinks': drinks, 'addons': addons, 'breakfast': breakfast, 'lunchmeal': lunchmeal, 'media_url':settings.MEDIA_URL}
     if request.method == 'POST':
         form = Menu(request.POST, request.FILES)
@@ -30,15 +28,37 @@ def index(request):
 
 
 def menu(request):
+    form = Orders()
     drinks = Menus.objects.filter(item_categories="drinks")
     addons = Menus.objects.filter(item_categories="addons")
     breakfast = Menus.objects.filter(item_categories="breakfast")
     lunchmeals = Menus.objects.filter(item_categories="lunchmeal")
     context = {'drinks': drinks, 'addons': addons, 'breakfast': breakfast, 'lunchmeals': lunchmeals, 'media_url':settings.MEDIA_URL}
+
+    
     if request.method == 'POST':
-        print('save')
-    else:
-        form = Menu()
+        # sample = request.POST.get("foods")
+        # sample1 = request.POST.get("prices")
+        # sample3 = request.POST.get("quantities")
+        foods = []
+        prices = []
+
+        for key, values in request.POST.lists():
+            if key == 'foods':
+                foods.append(values)
+        total = request.POST.get("total-purchase")
+
+        print(foods)
+        for food in foods:
+            
+            form.item = ' '.join(food)
+            form.save()
+        form.time = request.POST.get("hour")
+        form.item_status = 'SUCCESS'
+        form.total_purchase = total
+        form.save()
+
+
     
     return render(request, 'menu.html', context)
 
