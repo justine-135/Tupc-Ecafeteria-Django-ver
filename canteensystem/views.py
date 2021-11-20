2ae3ad6f28ac6faad3d0695e3c9047673dcd4b5e
Justine <ujusine@yahoo.com>
2021-11-20 16:43:16 +0800

    import menu from form

--- canteensystem/views.py
+++ canteensystem/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .form import BreakfastForm
+from .form import Menu
 from .models import Menus
 from django.conf import settings
 
 
 def index(request):
-    form = BreakfastForm()
+    form = Menu()
     datas = Menus.objects.all()
 
     #filter menu
@@ -18,12 +18,12 @@ def index(request):
     #add filtered here
     context = {'form': form, "foods": datas, 'drinks': drinks, 'addons': addons, 'breakfast': breakfast, 'lunchmeal': lunchmeal, 'media_url':settings.MEDIA_URL}
     if request.method == 'POST':
-        form = BreakfastForm(request.POST, request.FILES)
+        form = Menu(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('index')
     else:
-        form = BreakfastForm()
+        form = Menu()
 
     print(addons)
     return render(request, 'index.html', context)
@@ -35,6 +35,11 @@ def menu(request):
     breakfast = Menus.objects.filter(item_categories="breakfast")
     lunchmeals = Menus.objects.filter(item_categories="lunchmeal")
     context = {'drinks': drinks, 'addons': addons, 'breakfast': breakfast, 'lunchmeals': lunchmeals, 'media_url':settings.MEDIA_URL}
+    if request.method == 'POST':
+        print('save')
+    else:
+        form = Menu()
+    
     return render(request, 'menu.html', context)
 
 
