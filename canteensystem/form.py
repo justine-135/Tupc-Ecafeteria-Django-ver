2b0452e2309e9904bea4fba469db1a1dbe6080dc
louisesebastianeyagi <louisesebastiane.yagi@gsfe.tupcavite.edu.ph>
2021-11-22 02:24:05 +0800

    added onkey events on widgets

--- canteensystem/form.py
+++ canteensystem/form.py
@@ -7,6 +7,6 @@ class Menu(forms.ModelForm):
         model = Menus
         fields = [ 'item_categories', 'item_name', 'item_price', 'item_quantity', 'item_img',]
         widgets = {'item_categories': forms.Select(attrs={'id': 'dropdown'}),
-            'item_name': forms.TextInput(attrs={'placeholder': 'Enter food'}),
-        'item_price': forms.TextInput(attrs={'placeholder': 'Enter Price'}),
-        'item_quantity': forms.TextInput(attrs={'placeholder': 'Enter Quantity'})}
+            'item_name': forms.TextInput(attrs={'placeholder': 'Enter food', 'onkeypress': 'return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123) || (event.charCode==32)', 'onpaste':"return false", 'ondrag':"return false", 'ondrop':"return false"}),
+        'item_price': forms.TextInput(attrs={'placeholder': 'Enter Price', 'type': 'number', 'placeholder': 'Enter Price', 'onkeypress': 'return event.keyCode === 8 || event.charCode >= 48 && event.charCode <= 57', 'onpaste':"return false", 'ondrag':"return false", 'ondrop':"return false"}),
+        'item_quantity': forms.TextInput(attrs={'placeholder': 'Enter Quantity', 'type': 'number', 'placeholder': 'Enter Price', 'onkeypress': 'return event.keyCode === 8 || event.charCode >= 48 && event.charCode <= 57', 'onpaste':"return false", 'ondrag':"return false", 'ondrop':"return false"})}
