5bd3620bcbf126fdde50fd2e826225576dcd2700
Justine <ujusine@yahoo.com>
2021-11-27 17:52:52 +0800

    add url for cancel-order html

--- canteensystem/urls.py
+++ canteensystem/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('update/<str:pk>/', views.updateFood, name='update'),
     path('delete/<str:pk>/', views.deleteFood, name='delete'),
     path('cancel/<str:pk>/', views.cancelOrder, name='cancel-order'),
+    path('clear-inventory/', views.clearInventory, name='clear-inventory')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
