ccc45fe24f9214ff55c40c2058cc68ca43d90c08
Justine <ujusine@yahoo.com>
2021-12-09 22:35:15 +0800

    map login and registration page to url

--- canteensystem/urls.py
+++ canteensystem/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('update/<str:pk>/', views.updateFood, name='update'),
     path('delete/<str:pk>/', views.deleteFood, name='delete'),
     path('cancel/<str:pk>/', views.cancelOrder, name='cancel-order'),
-    path('clear-inventory/', views.clearInventory, name='clear-inventory')
+    path('clear-inventory/', views.clearInventory, name='clear-inventory'),
+    path('registration/', views.createAccount, name ='registration'),
+    path('login/', views.login, name ='login')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
