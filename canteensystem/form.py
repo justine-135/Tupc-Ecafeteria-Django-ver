4afcb0005b828f77e92e5a8cce51f6df3ae57c57
Justine <ujusine@yahoo.com>
2021-12-09 19:27:42 +0800

    create user registration form

--- canteensystem/form.py
+++ canteensystem/form.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.db.models import fields
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 
 class Menu(forms.ModelForm):
@@ -10,3 +13,8 @@ class Menu(forms.ModelForm):
             'item_name': forms.TextInput(attrs={'placeholder': 'Enter food', 'onkeypress': 'return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123) || (event.charCode==32)', 'onpaste':"return false", 'ondrag':"return false", 'ondrop':"return false"}),
         'item_price': forms.TextInput(attrs={'placeholder': 'Enter Price', 'type': 'number', 'onkeypress': 'return event.keyCode === 8 || event.charCode >= 48 && event.charCode <= 57', 'onpaste':"return false", 'ondrag':"return false", 'ondrop':"return false"}),
         'item_quantity': forms.TextInput(attrs={'placeholder': 'Enter Quantity', 'type': 'number', 'onkeypress': 'return event.keyCode === 8 || event.charCode >= 48 && event.charCode <= 57', 'onpaste':"return false", 'ondrag':"return false", 'ondrop':"return false"})}
+
+class CreateAccount(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
