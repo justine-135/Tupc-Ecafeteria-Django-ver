5f47a7afacc62ae8594bda4e63d328494eb424d2
Justine <ujusine@yahoo.com>
2022-01-19 00:12:04 +0800

    create custom user models

--- canteensystem/models.py
+++ canteensystem/models.py
@@ -1,21 +1,16 @@
 from django.db import models
-from django.db.models.fields import CharField, DateTimeField, IntegerField
+from django.db.models.fields import CharField,IntegerField
 from django.db.models.fields.files import ImageField
+from django.contrib.auth.models import AbstractUser
 
-
-FOOD_CATEGORY = (
-    ('breakfast','Breakfast'),
-    ('lunchmeal', 'Lunchmeal'),
-    ('drinks','Beverage and Drinks'), 
-    ('addons','Add-ons'),
-)
-
-# TRUE_FALSE_CHOICES = (
-#     (True, 'Yes'),
-#     (False, 'No')
-# )
 
 class Menus(models.Model):
+    FOOD_CATEGORY = (
+        ('breakfast','Breakfast'),
+        ('lunchmeal', 'Lunchmeal'),
+        ('drinks','Beverage and Drinks'), 
+        ('addons','Add-ons'),   
+    )
     item_categories = CharField(max_length=20, choices=FOOD_CATEGORY, default='breakfast', blank=False)
     item_name = CharField(max_length=30, verbose_name="item_name", blank=False)
     item_quantity = IntegerField(verbose_name="item_quantity", blank=False)
@@ -30,4 +25,14 @@ class Orders(models.Model):
     item = CharField(max_length=30, verbose_name="item", blank=False)
     total_purchase = CharField(max_length=5, verbose_name="total_purchase", blank=False)
     item_status = CharField(max_length=30, verbose_name="item_status", blank=False)
+
+class CustomUser(AbstractUser):
+    # first_name = CharField(max_length=30, verbose_name="first_name", blank=True)
+    # last_name = CharField(max_length=30, verbose_name="last_name" , blank=True)
+    # user_name = CharField(max_length=30, verbose_name="username", blank=True)
+    # password1 = CharField(max_length=30, verbose_name="password1", blank=True )
+    # password2 = CharField(max_length=30, verbose_name="password2", blank=True)
+
+    is_customer = models.BooleanField(default=False)
+    is_admins = models.BooleanField(default=False)
 
