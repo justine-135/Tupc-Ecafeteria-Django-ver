4656d4584c1a228f7bb35c9e14bf5beea8f4a6ff
Justine <ujusine@yahoo.com>
2022-01-26 00:03:37 +0800

    restrict user from other pages. grant access and permission by superadmin

--- canteensystem/views.py
+++ canteensystem/views.py
@@ -20,146 +20,177 @@ def index(request):
     lunchmeal = Menus.objects.filter(item_categories="lunchmeal")
     
     name = request.user.email
-    print(form)
     context = {'form': form, "foods": datas, 'drinks': drinks, 'addons': addons, 'breakfast': breakfast, 'lunchmeal': lunchmeal, 'name': name, 'media_url':settings.MEDIA_URL}
     if request.method == 'POST':
-        form = Menu(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
+        if request.user.is_authenticated:
+            if request.user.inventory_create or request.user.is_superuser:
+                form = Menu(request.POST, request.FILES)
+                if form.is_valid():
+                    form.save()
+                    return redirect('index')
+            else:
+                return redirect('index')
     else:
         form = Menu()
 
     return render(request, 'index.html', context)
 
+
 @login_required(login_url='login')
 def menu(request):
-    if request.user.is_authenticated and request.user.is_admins:
-        return redirect('index')
-    
-    form = Orders()
-    drinks = Menus.objects.filter(item_categories="drinks")
-    addons = Menus.objects.filter(item_categories="addons")
-    breakfast = Menus.objects.filter(item_categories="breakfast")
-    lunchmeals = Menus.objects.filter(item_categories="lunchmeal")
-    name = request.user.email
+    if request.user.is_authenticated:
+        if request.user.is_superuser or request.user.menu_create:
+            form = Orders()
+            drinks = Menus.objects.filter(item_categories="drinks")
+            addons = Menus.objects.filter(item_categories="addons")
+            breakfast = Menus.objects.filter(item_categories="breakfast")
+            lunchmeals = Menus.objects.filter(item_categories="lunchmeal")
+            name = request.user.email
 
-    context = {'drinks': drinks, 'addons': addons, 'breakfast': breakfast, 'lunchmeals': lunchmeals, 'name': name, 'media_url':settings.MEDIA_URL}
+            context = {'drinks': drinks, 'addons': addons, 'breakfast': breakfast, 'lunchmeals': lunchmeals, 'name': name, 'media_url':settings.MEDIA_URL}
 
-    if request.method == 'POST':
-        foods = []
+            if request.method == 'POST':
+                foods = []
 
-        for key, values in request.POST.lists():
-            if key == 'foods':
-                foods.append(values)
-        total = request.POST.get("total-purchase")
+                for key, values in request.POST.lists():
+                    if key == 'foods':
+                        foods.append(values)
+                total = request.POST.get("total-purchase")
 
-        for food in foods:
-            form.item = ' '.join(food)
-            form.save()
-            
-        form.time = request.POST.get("hour")
-        form.item_status = 'SUCCESS'
-        form.total_purchase = total
-        form.save()
+                for food in foods:
+                    form.item = ' '.join(food)
+                    form.save()
+                    
+                form.time = request.POST.get("hour")
+                form.item_status = 'SUCCESS'
+                form.total_purchase = total
+                form.save()
 
-    return render(request, 'menu.html', context)
+            return render(request, 'menu.html', context)
+
+        else:
+            return redirect('index')
 
 @login_required(login_url='login')
 def inventory(request):
-    if request.user.is_authenticated and request.user.is_admins:
-        return redirect('index')
-
-    name = request.user.email
-
-    order = Orders.objects.all()
-    context = {'order': order, 'name': name}
-    return render(request, 'inventory.html', context)
+    if request.user.is_authenticated:
+        if request.user.orders_delete or request.user.orders_update or request.user.is_superuser:
+            name = request.user.email
+            order = Orders.objects.all()
+            context = {'order': order, 'name': name}
+            return render(request, 'inventory.html', context)
+        else:
+            return redirect('index')
 
 @login_required(login_url='login')
 def admins(request):
-    if request.user.is_authenticated and request.user.is_admins:
-        return redirect('index')
-    name = request.user.email
-
-    users = CustomUser.objects.all()
-    context = {'users': users, 'name': name}
-    return render(request, 'users.html', context)
+    if request.user.is_authenticated:
+        if request.user.is_superuser or request.user.admins_create or request.user.admins_update:
+            name = request.user.email
+            users = CustomUser.objects.all()        
+            print(CustomUser.admins_create)
+            context = {'users': users, 'name': name}
+            return render(request, 'users.html', context)
+        else:
+            return redirect('index')
 
 @login_required(login_url='login')
 def permission(request, pk):
-    datas = CustomUser.objects.get(id=pk)
-    form = CreateAccount(instance=datas)
+    if request.user.is_authenticated:
+        if request.user.is_superuser or request.user.admins_update:    
+            datas = CustomUser.objects.get(id=pk)
+            email = request.user.email
+            uemail = datas.email
+            name = datas.username
+            uid = datas.id
+            if request.method == 'POST':
+                datas.admins_create = request.POST.get('admins_create')
+                datas.admins_update = request.POST.get('admins_update')
+                datas.orders_update = request.POST.get('orders_update')
+                datas.orders_delete = request.POST.get('orders_delete')
+                datas.inventory_create = request.POST.get('inventory_create')
+                datas.inventory_update = request.POST.get('inventory_update')
+                datas.inventory_delete = request.POST.get('inventory_delete')
+                datas.menu_create = request.POST.get('menu_create')
+                datas.save()
+                return redirect('admin-accounts')
 
-    if request.method == 'POST':
-        datas.admins_create = request.POST.get('admins_create')
-        datas.admins_read = request.POST.get('admins_read')
-        datas.admins_update = request.POST.get('admins_update')
-        datas.orders_read = request.POST.get('orders_read')
-        datas.orders_update = request.POST.get('orders_update')
-        datas.orders_delete = request.POST.get('orders_delete')
-        datas.inventory_create = request.POST.get('inventory_create')
-        datas.inventory_read = request.POST.get('inventory_read')
-        datas.inventory_update = request.POST.get('inventory_update')
-        datas.inventory_delete = request.POST.get('nventory_delete')
-        datas.menu_create = request.POST.get('menu_create')
-        datas.menu_read = request.POST.get('menu_read')
-        datas.save()
-        return redirect('admin-accounts')
+            context = {'datas': datas, 'name': name, 'email':email, 'uid': uid, 'uemail': uemail}
 
-    context = {'datas': datas}
+            return render(request, 'permissions.html/', context)
 
-    return render(request, 'permissions.html/', context)
+        else:
+            return redirect('index')
+
+    else:
+        return redirect('index')
 
 
 @login_required(login_url='login')
 def updateFood(request, pk):
-    datas = Menus.objects.get(id=pk)
-    form = Menu(instance=datas)
-    drinks = Menus.objects.filter(item_categories="drinks")
-    addons = Menus.objects.filter(item_categories="addons")
-    breakfast = Menus.objects.filter(item_categories="breakfast")
-    lunchmeals = Menus.objects.filter(item_categories="lunchmeal")
-    if request.method == 'POST':
-        form = Menu(request.POST, request.FILES, instance=datas)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
+    if request.user.is_authenticated:
+        if request.user.inventory_update or request.user.is_superuser:
+            datas = Menus.objects.get(id=pk)
+            form = Menu(instance=datas)
+            name = request.user.email
 
-    context = {'form': form, 'drinks': drinks, 'addons': addons, 'breakfast': breakfast, 'lunchmeals': lunchmeals, 'media_url':settings.MEDIA_URL}
+            drinks = Menus.objects.filter(item_categories="drinks")
+            addons = Menus.objects.filter(item_categories="addons")
+            breakfast = Menus.objects.filter(item_categories="breakfast")
+            lunchmeals = Menus.objects.filter(item_categories="lunchmeal")
+            if request.method == 'POST':
+                form = Menu(request.POST, request.FILES, instance=datas)
+                if form.is_valid():
+                    form.save()
+                    return redirect('index')
 
-    return render(request, 'update.html/', context)
+            context = {'form': form, 'drinks': drinks, 'addons': addons, 'breakfast': breakfast, 'lunchmeals': lunchmeals, 'name':name, 'media_url':settings.MEDIA_URL}
+
+            return render(request, 'update.html/', context)
+
+        return redirect('index')
 
 @login_required(login_url='login')
 def deleteFood(request, pk):
-    datas = Menus.objects.get(id=pk)
-    if request.method == "POST":
-        datas.delete() 
-        return redirect('index')
+    if request.user.is_authenticated:
+        if request.user.inventory_delete or request.user.is_superuser:
+            datas = Menus.objects.get(id=pk)
+            if request.method == "POST":
+                datas.delete() 
+                return redirect('index')
+        else:
+            return redirect('index')
 
-    context = {'item':datas, 'media_url':settings.MEDIA_URL}
-    return render(request, 'delete.html/', context)
+        context = {'item':datas, 'media_url':settings.MEDIA_URL}
+        return render(request, 'delete.html/', context)
 
 @login_required(login_url='login')
 def cancelOrder(request, pk):
-    datas = Orders.objects.get(id=pk)
-    if request.method == "POST":
-        datas.item_status = "CANCELLED"
-        datas.save()
-        return redirect('inventory')
-    context = {'item': datas}
+    if request.user.is_authenticated:
+        if request.user.is_superuser or request.user.orders_update:
+            datas = Orders.objects.get(id=pk)
+            if request.method == "POST":
+                datas.item_status = "CANCELLED"
+                datas.save()
+                return redirect('inventory')
+        else:
+            return redirect('/orders')
 
-    return render(request, 'cancel-order.html/', context)
+        context = {'item': datas}
+        return render(request, 'cancel-order.html/', context)
 
 @login_required(login_url='login')
 def clearInventory(request):
-    datas = Orders.objects.all()
-    if request.method == "POST":
-        datas.delete()
-        return redirect('inventory')
+    if request.user.is_authenticated:
+        if request.user.is_superuser or request.user.orders_delete:
+            datas = Orders.objects.all()
+            if request.method == "POST":
+                datas.delete()
+                return redirect('inventory')
+        else:
+            return redirect('index')
 
     context = {'item':datas}
-
     return render(request, 'clear-inventory.html', context)
 
 def loginAccount(request):
@@ -188,57 +219,57 @@ def loginAccount(request):
 
 @login_required(login_url='login')
 def createAccount(request):
-    if request.user.is_authenticated and request.user.is_admins:
-        return redirect('index')
+    if request.user.is_authenticated:
+        if request.user.is_superuser or request.user.is_admins_create:
+            form = CreateAccount()
+            if request.method == "POST":
+                form = CreateAccount(request.POST)
+                fname = request.POST.get('first_name')
+                lname = request.POST.get('last_name')
+                password = request.POST.get('password1')
+                password2 = request.POST.get('password2')
+                user = request.POST.get('username')
 
-    else:
-        form = CreateAccount()
-        if request.method == "POST":
-            form = CreateAccount(request.POST)
-            fname = request.POST.get('first_name')
-            lname = request.POST.get('last_name')
-            password = request.POST.get('password1')
-            password2 = request.POST.get('password2')
-            user = request.POST.get('username')
+                min = 8
+                regex = re.compile('[.@_!#$%^&*()<>?/\|}{~:]')
 
-            min = 8
-            regex = re.compile('[.@_!#$%^&*()<>?/\|}{~:]')
+                if form.is_valid():
+                    if fname == '' or lname == '':
+                        messages.info(request, "Fill all forms.")
 
-            if form.is_valid():
-                if fname == '' or lname == '':
-                    messages.info(request, "Fill all forms.")
+                    elif regex.search(user) != None or regex.search(fname) != None or regex.search(lname) != None or regex.search(password) != None:
+                        messages.info(request, "Symbols and special characters not allowed.")
 
-                elif regex.search(user) != None or regex.search(fname) != None or regex.search(lname) != None or regex.search(password) != None:
-                    messages.info(request, "Symbols and special characters not allowed.")
+                    else:
+                        form.instance.is_staff = True
+                        form.save()
 
-                else:
-                    form.instance.is_staff = True
-                    form.save()
-
-                    return redirect('admin-accounts')
-
-            else:
-                exist = CustomUser.objects.filter(username=user).exists()
-                if fname == '' or lname == '' or password == '' or password2 == '' or user == '':
-                    messages.info(request, "Fill all forms.")
-                    
-                elif password != password2:
-                    messages.info(request, "Password does not match. Please try again.")
-
-                elif exist:
-                    messages.info(request, "Username already exist.")
-
-                elif len(password) < min:
-                    messages.info(request, "Password must be at least 8 characters long.")
+                        return redirect('admin-accounts')
 
                 else:
-                    messages.info(request, "Password must have:")
-                    messages.info(request, "Lower and uppercase characters.")
-                    messages.info(request, "At least single number.")
+                    exist = CustomUser.objects.filter(username=user).exists()
+                    if fname == '' or lname == '' or password == '' or password2 == '' or user == '':
+                        messages.info(request, "Fill all forms.")
+                        
+                    elif password != password2:
+                        messages.info(request, "Password does not match. Please try again.")
 
-        context = {'form': form}
+                    elif exist:
+                        messages.info(request, "Username already exist.")
 
-        return render(request, 'accounts/registration.html', context)
+                    elif len(password) < min:
+                        messages.info(request, "Password must be at least 8 characters long.")
+
+                    else:
+                        messages.info(request, "Password must have:")
+                        messages.info(request, "Lower and uppercase characters.")
+                        messages.info(request, "At least single number.")
+
+            context = {'form': form}
+
+            return render(request, 'accounts/registration.html', context)
+        else:
+            return redirect('index')
 
 def logoutAccount(request):
     logout(request)
