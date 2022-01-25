89ee2f370bd93452cf94a154f515163ba530d337
louisesebastianeyagi <louisesebastiane.yagi@gsfe.tupcavite.edu.ph>
2022-01-26 03:11:13 +0800

    removed slash from domain name

--- elective2/settings.py
+++ elective2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5#+*2ngsfjbaw+i=gnisa88$80@ci9&mzp=lc8k%mpx*b@z#_l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','tupce-cafeteria.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1:8000','tupce-cafeteria.herokuapp.com']
 
 
 # Application definition
